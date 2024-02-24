cd64958178475faab5567d5e416bc6fe0d992e97
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-02-24 15:58:42 +0100

    test file is updated in src

--- src/config.py
+++
@@ -1,5 +0,0 @@
-RAW_PATH = "./data/raw/"
-PROCESSED_PATH = "./data/processed/"
-MODELS_CHECKPOINTS = "./checkpoints/models/"
-BEST_MODEL_PATH = "./checkpoints/best_model/"
-SAVE_IMAGE_PATH = "./outputs/fake_images/"
